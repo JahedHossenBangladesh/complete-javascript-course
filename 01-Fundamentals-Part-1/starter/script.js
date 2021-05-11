13e2c13ed132d3dc3a2e52d5fefb33136cc13276
JahedHossenBangladesh <jahedhossen4358.com>
2021-05-12 03:09:34 +0600

    this is basic

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,36 @@
+/*let js = "amazing";
+if (js === "amazing") alert("The js is Nice");
+
+console.log(2 + 2 +2 );
+console.log("jahed");
+ let name = "jahed";
+
+ console.log(name);
+
+
+
+let jsFun = true;
+console.log(jsFun);
+
+console.log(typeof true);
+console.log(typeof jsFun);
+console.log(typeof "jahed");
+console.log(typeof 33);
+
+jsFun = 'yes!';
+
+console.log(jsFun);
+
+let year;
+console.log(typeof year);
+year = 3333;
+console.log(year);
+console.log(typeof null)
+
+let age = 30;
+age = 43;
+const birthYear = 1993;
+
+lastName = "hossen";
+console.log(lastName);
+*/
